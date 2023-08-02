96d5447e9abb3ee513673e1a54a250a59d789f27
daniel-vid <daniel@danielvidal.dev>
2023-08-02 03:04:47 +0300

    deprecated lsp config

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -2,11 +2,6 @@ local lsp = require("lsp-zero")
 
 lsp.preset("recommended")
 
-lsp.ensure_installed({
-  'tsserver',
-  'rust_analyzer',
-})
-
 -- Fix Undefined global 'vim'
 lsp.nvim_workspace()
 
@@ -17,7 +12,7 @@ local cmp_mappings = lsp.defaults.cmp_mappings({
   ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
   ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
   ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-  ["<C-Space>"] = cmp.mapping.complete(),
+  ["<C-b>"] = cmp.mapping.complete(),
 })
 
 cmp_mappings['<Tab>'] = nil
