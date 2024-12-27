ee675b3c579c3e64b33950d81eac15e5ceb6fbdf
daniel-vid <daniel@danielvidal.dev>
2024-12-27 14:38:10 +0200

    add harpoon plugin and improve oil to watch changes

--- lua/theprimeagen/lazy/oil.lua
+++ lua/theprimeagen/lazy/oil.lua
@@ -2,16 +2,17 @@ return {
     "stevearc/oil.nvim",
     config = function()
         require("oil").setup({
-                            columns = {
-                    "icon",
-                    -- "permissions",
-                    -- "size",
-                    -- "mtime",
-                },
-                view_options = {
-                    -- Show files and directories that start with "."
-                    show_hidden = true,
-                }
+            columns = {
+                "icon",
+                -- "permissions",
+                -- "size",
+                -- "mtime",
+            },
+            view_options = {
+                -- Show files and directories that start with "."
+                show_hidden = true,
+            },
+            watch_for_changes = true,
         })
     end
 }
