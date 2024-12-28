c2c80ddc3ba034581375a0aaebbd633b649ed9d3
daniel-vid <daniel@danielvidal.dev>
2024-12-28 15:18:53 +0200

    small refactor
    - change structure
    - add auto format on save
    - add lazygit plugin

--- lazy_init.lua
+++ lazy_init.lua
@@ -12,6 +12,9 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    spec = "theprimeagen.lazy",
+    spec = {
+        -- import your plugins
+        { import = "plugins" },
+    },
     change_detection = { notify = false }
 })
